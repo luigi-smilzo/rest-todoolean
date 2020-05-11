9b7d4680d4dde8e6d3b16fe4d1ea9eeff6704322
Luigi <luigismilzo@live.com>
2020-05-11 16:23:25 +0200

    Setup

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    
+    // References
+    var input = $('#Input');
+    var button = $('#Button');
+    var container = $('.TaskList');
+
+    // Handlebars Init
+    var source = $('#list-template').html();
+    var template = Handlebars.compile(source);
+
+
+    
+
+}); // <-- End ready
+
+
+/************
+ * FUNCTIONS
+ ***********/
+
